a6d7e7df0bc5aa6663715254cfb58b512cc1333c
Daniel Pino <danielpino@Daniels-MacBook-Pro.local>
2018-02-09 16:28:17 -0500

    Johnatheen is being annoying

---
+++ app/controllers/api/songs_controller.rb
@@ -0,0 +1,40 @@
+class Api::SongsController < ApplicationController
+
+    def index
+        # @songs = Artist.find(params[:artist_id]).songs
+        @song = Song.where(artist_id: params[:artist_id])
+        render json: @song
+    end
+
+    def show
+        @song = Song.find(params[:id])
+        render json: @song
+    end
+
+    def create
+        @song = Song.create!(song_params, params[:artist_id] )
+        render json: @song
+    end
+
+    def update
+        @song = Song.find(params[:id])
+        @song.update!(song_params)
+        render json: @song
+    end
+
+    def destroy
+        @song = Song.find(params[:id])
+        @song.destroy
+
+        render status: :ok
+    end
+
+    private
+    def song_params
+        params
+            .require(:song)
+            .permit(:title, :album, :preview_url, :artwork)
+    end
+
+    
+end
