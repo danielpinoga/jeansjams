300f397c51b86140c5e1020b13da715a8dc94d0a
Daniel Pino <danielpino@Daniels-MacBook-Pro.local>
2018-02-09 13:52:59 -0500

    all APIs done

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,6 +1,39 @@
 class Api::ArtistsController < ApplicationController
-  def index
-    @artists = Artist.all
-    render json: @artists
-  end
+
+    def index
+        @artists = Artist.all
+        render json: @artists
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+        render json: @artist
+    end
+
+    def create
+        @artist = Artist.create!(artist_params)
+        render json: @artist
+    end
+
+    def update
+        @artist = Artist.find(params[:id])
+        @artist.update!(artist_params)
+        render json: @artist
+    end
+
+    def destroy
+        @artist = Artist.find(params[:id])
+        @artist.destroy
+
+        render status: :ok
+    end
+
+    private
+    def artist_params
+        params
+            .require(:artist)
+            .permit(:name, :nationality, :photo_url)
+    end
+
+    
 end
